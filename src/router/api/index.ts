d1f5f07ac8f21f3a83e190929a99c75480663f0c
Kim Jioo <jwkim101201@gmail.com>
2021-06-13 15:03:41 +0900

    feat: add checkToken router use

--- src/router/api/index.ts
+++ src/router/api/index.ts
@@ -4,7 +4,11 @@ import { postGroups } from './routes/groups'
 import register from './routes/register'
 import token from './routes/token'
 
+import checkToken from './middlewares/checkToken'
+
 const router = Router()
+
+router.use(checkToken)
 
 router.post('/gropus', postGroups)
 router.post('/register', register)
