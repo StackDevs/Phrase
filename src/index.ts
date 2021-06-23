4cbeb9edb7f14723a8dd7e611e1ea09d6a007411
Kim Jioo <jwkim101201@gmail.com>
2021-06-23 19:14:31 +0900

    feat: add ws token checking

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import jwt, { VerifyErrors } from 'jsonwebtoken'
+import fs from 'fs'
 
 import { createServer } from 'http'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 
 import api from './router/api'
 
@@ -9,13 +11,21 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
-io.on('connection', (socket) => {
+const KEY = fs.readFileSync('../resources/private.key').toString('utf8')
+
+io.on('connection', socketHandler)
+
+function socketHandler (socket: Socket) {
   if (socket.handshake.headers['authorization']) {
-    // TODO: check token
+    const { authorization: token } = socket.handshake.headers
+
+    jwt.verify(token, KEY, (err: VerifyErrors | null) => {
+      if (err) return socket.disconnect()
+    })
   } else {
     socket.disconnect()
   }
-})
+}
 
 app.use(express.static('../resources/static'))
 app.use(express.json())
